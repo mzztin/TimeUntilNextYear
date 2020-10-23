9d7677c9982fc2154226ed0ae797b30ffa90c65b
martin @ exze <exze.real@gmail.com>
2020-10-23 21:50:03 +0200

    Final updates

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import './App.css';
-import { createFormat } from "../utils"
+import { createFormat, getTimeLeft } from "../utils"
 
 
 export default class App extends React.Component {
   render() {
     return (
-      <div className="comonent-app">
-        New Year in: {createFormat()}
+      <div className="component-app">
+        <div className="text">
+          <h2>Time until next year</h2>
+        </div>
+
+        <div className="timer">
+          <p>
+            {getTimeLeft().days} days
+          </p>
+
+          <p>
+            {getTimeLeft().hours} hours
+          </p>
+          
+          <p>
+            {getTimeLeft().minutes} minutes
+          </p>
+          
+          <p>
+            {getTimeLeft().seconds} seconds
+          </p>
+
+        </div>
       </div>
     )
   }
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,7 @@
-function getTimeLeft(deadline: Date | undefined = undefined) {
-  const date: Date = new Date((deadline ? deadline.getTime() : new Date("January 1, 2021 00:00:00").getTime()) - (new Date().getTime()))
+export const new_year: string = "January 1, 2021 00:00:00";
+
+export function getTimeLeft(deadline: Date | undefined = undefined) {
+  const date: Date = new Date((deadline ? deadline.getTime() : new Date(new_year).getTime()) - (new Date().getTime()))
   
   return {
     'days': Math.floor(date.getTime()/(1000*60*60*24)),
